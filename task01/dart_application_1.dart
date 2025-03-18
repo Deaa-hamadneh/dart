b786c8bd34192d0288b3e4c02d4287815b51023b
Deaa-hamadneh <deaahamadneh@icloud.com>
2025-03-18 08:05:59 +0300

    Add files via upload

---
+++ task01/dart_application_1.dart
@@ -0,0 +1,14 @@
+void main(List<String> args){
+/*int age = 25;
+print(age);
+double pi =3.14150;
+print(pi);
+*/
+String name ='Alice';
+double height=1.65;
+print(name);
+print(height);
+print ("meter tall");
+
+
+}
